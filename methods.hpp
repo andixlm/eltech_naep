d2be5da74a159ad845dc8586def1b02d6381047e
andixlm <andixev@gmail.com>
2017-10-30 23:29:38 +0300

    methods: Declare implemented interpolation extrapolation method

--- methods.hpp
+++ methods.hpp
@@ -27,7 +27,9 @@ double newton(double (* df)(const double), double (* ddf)(const double),
     const double initial, const double epsilon);
 double linear_interpolation(double (* df)(const double),
     double& left_bound, double& right_bound, const double epsilon);
-    
+
+double interpolation_extrapolation(double (* f)(const double),
+    const double initial, const double epsilon);
 double powell(double (* f)(const double),
     double& left_bound, double& right_bound, const double epsilon);
 
