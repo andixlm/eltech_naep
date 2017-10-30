82e280fce0c13f206a48d657863ecf2a5c61fbd5
andixlm <andixev@gmail.com>
2017-10-30 22:47:57 +0300

    methods: Implement sven by derivative method

--- methods.cpp
+++ methods.cpp
@@ -39,3 +39,40 @@ void sven_value(double (* f)(const double), const double initial,
         right_bound = prev;
     }
 }
+
+void sven_derivative(double (* df)(const double), const double initial,
+    double& left_bound, double& right_bound)
+{
+    double step;
+    double prev, curr;
+
+    step = 0.01;
+
+    if (init != 0.0)
+        step *= fabs(init);
+
+    if (df(init) > 0.0)
+        step = -step;
+
+    prev = init;
+    curr = init + step;
+
+    while (copysignf(1.0, df(prev)) * copysignf(1.0, df(curr)) > 0.0)
+    {
+        step *= 2.0;
+
+        prev = curr;
+        curr += step;
+    }
+
+    if (prev < curr)
+    {
+        left_bound = prev;
+        right_bound = curr;
+    }
+    else
+    {
+        left_bound = curr;
+        right_bound = prev;
+    }
+}
--- methods.hpp
+++ methods.hpp
@@ -3,5 +3,7 @@
 
 void sven_value(double (* f)(const double), const double initial,
     double& left_bound, double& right_bound);
+void sven_derivative(double (* df)(const double), const double initial,
+    double& left_bound, double& right_bound);
 
 #endif
