c75a78859108a9a21e1c4ddb565bb7b1d5f0db40
andixlm <andixev@gmail.com>
2017-11-05 23:48:20 +0300

    app: Implement initial ui

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include <QHBoxLayout>
+#include <QLabel>
 #include <QMainWindow>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QWidget>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +15,30 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
+
+private:
+    constexpr static const char* MATH_FUNCTION_LABEL = "Math function:";
+    static const unsigned MATH_FUNCTION_HEIGHT = 24;
+
+    constexpr static const char* LOG_LABEL = "Log:";
+
+    QWidget mMainWidget;
+
+    QHBoxLayout mMainLayout;
+
+    QVBoxLayout mParametersLayout;
+
+    QVBoxLayout mMathFunctionLayout;
+    QLabel mMathFunctionLabel;
+    QTextEdit mMathFunction;
+
+    QVBoxLayout mInfoLayout;
+
+    QVBoxLayout mLogLayout;
+    QLabel mLogLabel;
+    QTextEdit mLog;
+
+    void configureWindow();
 };
 
 #endif // MAINWINDOW_HPP
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,49 @@
 #include "mainwindow.hpp"
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+      mMainWidget(this),
+      mMainLayout(&mMainWidget)
 {
+    setCentralWidget(&mMainWidget);
+    configureWindow();
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::configureWindow()
+{
+    mMainLayout.setAlignment(Qt::AlignVCenter | Qt::AlignHCenter);
+
+    /* Parameters layout */
+    mMainLayout.addLayout(&mParametersLayout);
+
+    // Math formula
+    mParametersLayout.addLayout(&mMathFunctionLayout);
+    mParametersLayout.setAlignment(&mMathFunctionLayout, Qt::AlignTop);
+
+    mMathFunctionLayout.addWidget(&mMathFunctionLabel);
+    mMathFunctionLayout.setAlignment(&mMathFunctionLabel, Qt::AlignTop);
+    mMathFunctionLabel.setText(MATH_FUNCTION_LABEL);
+
+    mMathFunctionLayout.addWidget(&mMathFunction);
+    mMathFunctionLayout.setAlignment(&mMathFunction, Qt::AlignTop);
+    mMathFunction.setSizePolicy(QSizePolicy::Policy::Fixed,
+                                QSizePolicy::Policy::Fixed);
+    mMathFunction.setFixedHeight(MATH_FUNCTION_HEIGHT);
+
+    /* Info layout */
+    mMainLayout.addLayout(&mInfoLayout);
+
+    // Log output
+    mInfoLayout.addLayout(&mLogLayout);
+    mInfoLayout.setAlignment(&mLogLayout, Qt::AlignTop);
+
+    mLogLayout.addWidget(&mLogLabel);
+    mLogLabel.setText(LOG_LABEL);
+
+    mLogLayout.addWidget(&mLog);
 }
